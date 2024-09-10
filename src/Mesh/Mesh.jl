d641b9d537a119375754e15c4d376ff5c5ef0af8
Alejandro Morales Sierra <alejandro.moralessierra@wur.nl>
2024-09-10 11:04:44 +0200

    Fix bug in update_normals when updating meshes

--- src/Mesh/Mesh.jl
+++ src/Mesh/Mesh.jl
@@ -183,10 +183,10 @@ function update_normals!(m::Mesh)
         end
     elseif length(normals(m)) != div(length(vertices(m)), 3)
         ln = length(m.normals)
-        for i in 3ln:3:length(m.vertices)
-            @inbounds v1 = m.vertices[i]
-            @inbounds v2 = m.vertices[i+1]
-            @inbounds v3 = m.vertices[i+2]
+        for i in 3ln:3:(length(m.vertices) - 3)
+            @inbounds v1 = m.vertices[i + 1]
+            @inbounds v2 = m.vertices[i + 2]
+            @inbounds v3 = m.vertices[i + 3]
             n = normalize(cross(v2 - v1, v3 - v1))
             push!(m.normals, n)
         end
